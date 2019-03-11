1f582fff7bbac0533258592aeeeb5cbd6f8ef327
clintgitahi <clinton@yumkenya.com>
2019-03-11 07:21:54 +0300

    reading the response json and adding to Article class

--- NewsApp/ArticleTableViewController.swift
+++ NewsApp/ArticleTableViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ArticleTableViewController.swift
-//  NewsApp
-//
-//  Created by clinton gitahi on 11/03/2019.
-//  Copyright © 2019 clinton gitahi. All rights reserved.
-//
 
 import UIKit
 
--- NewsApp/NewsHelper.swift
+++ NewsApp/NewsHelper.swift
@@ -7,7 +7,35 @@ class NewsHelper {
     func getArticles() {
         Alamofire.request("https://newsapi.org/v2/top-headlines?country=us&apiKey=1f22b159cfbf404fa0216568c18827e2")
             .responseJSON{(response) in response
-                print(response)
+                
+                if let json = response.result.value as? [String:Any] {
+                    if let jsonArticles = json["articles"] as? [[String: Any]] {
+                        var articles = [Article]()
+                        for jsonArticle in jsonArticles {
+                            guard let title = jsonArticle["title"] as? String,
+                            let urlToImage = jsonArticle["urlToImage"] as? String,
+                            let url = jsonArticle["url"] as? String,
+                            let description = jsonArticle["description"] as? String
+                                else {
+                                    continue
+                            }
+                            let article = Article()
+                            article.title = title
+                            article.urlToImage = urlToImage
+                            article.url = url
+                            article.description = description
+                            articles.append(article)
+                        }
+                    }
+                }
         }
     }
+}
+
+class Article {
+    var title = ""
+    var urlToImage = ""
+    var url = ""
+    var description = ""
+    var category = ""
 }
