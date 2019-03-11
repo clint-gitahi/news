9704c5d2c01b74559f11e5977fee6c1ce5554e6f
clintgitahi <clinton@yumkenya.com>
2019-03-11 04:51:35 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by clinton gitahi on 11/03/2019.
+//  Copyright © 2019 clinton gitahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
