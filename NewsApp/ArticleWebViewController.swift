3786164adc1c312e85d6f77a6bdc35d1dbe18141
clintgitahi <clinton@yumkenya.com>
2019-03-12 06:22:58 +0300

    getting webview to work

--- NewsApp/ArticleWebViewController.swift
+++ NewsApp/ArticleWebViewController.swift
@@ -1,14 +1,18 @@
 
 import UIKit
+import WebKit
 
 class ArticleWebViewController: UIViewController {
 
+    @IBOutlet weak var articleWebView: WKWebView!
     var article = Article()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        if let url = URL(string: article.url) {
+            articleWebView.load(URLRequest(url: url))
+        }
     }
 
 }
